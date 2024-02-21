000f459abe4e9ce8fb79a341438b5283845655e4
TheRealLemonke <nicolasputra36@gmail.com>
2024-02-21 15:18:56 +0700

    Player Movement
    Tambahan player dan prototyping scene. Gerak WASD/Left Stick gamepad buat gerakan. Space/A,X,dll buat lompat.

---
+++ Keraton Climb/Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+
+    void FixedUpdate()
+    {
+        // tell playermotor to move from the value of our movement action
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        // tell playerlook to rotate from the value of our look action
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
